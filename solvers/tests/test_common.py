dbcb3012c7120be2adc2d1d086b220caa0d4d29b
jasieksz <jasiek.sznajd@gmail.com>
2025-08-26 21:11:43 +0200

    feat: setup pytest

---
+++ solvers/tests/test_common.py
@@ -0,0 +1,22 @@
+import pytest
+from muoblp.model.multi_objective_lp import MultiObjectiveLpProblem
+
+from muoblpsolvers.common import get_total_budget_constraint
+
+
+def test_get_total_budget_constraint_throws_missing_pb(empty_pb: MultiObjectiveLpProblem):
+    # when
+    with pytest.raises(Exception) as err:
+        _ = get_total_budget_constraint(empty_pb)
+
+    # then
+    assert 'Problem does not have PB constraint' in str(err.value)
+
+
+def test_get_total_budget_constraint(basic_pb: MultiObjectiveLpProblem):
+    # when
+    constraint = get_total_budget_constraint(basic_pb)
+
+    # then
+    assert constraint == "1"
+
