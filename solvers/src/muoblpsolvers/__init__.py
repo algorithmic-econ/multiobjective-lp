5e13bc1f057a247ae2cd71aefffbcef0458b13e3
Grzegorz Kwacz <gkwacz@hotmail.com>
2026-06-04 16:00:51 +0200

    Move mes to submodule

--- solvers/src/muoblpsolvers/__init__.py
+++ solvers/src/muoblpsolvers/__init__.py
@@ -2,22 +2,29 @@ import logging
 
 from muoblpsolvers.expanding_approvals import ExpandingApprovals
 from muoblpsolvers.greedy_solver import GreedySolver
-from muoblpsolvers.method_of_equal_shares_add1_solver import (
+from muoblpsolvers.mes import (
     MethodOfEqualSharesAdd1Solver,
-)
-from muoblpsolvers.method_of_equal_shares_constrains_solver import (
     MethodOfEqualSharesConstrainsSolver,
-)
-from muoblpsolvers.method_of_equal_shares_exponential_solver import (
     MethodOfEqualSharesExponentialSolver,
-)
-from muoblpsolvers.method_of_equal_shares_utility_solver import (
     MethodOfEqualSharesUtilitySolver,
 )
 from muoblpsolvers.phragmen import PhragmenSolver
 from muoblpsolvers.single_transferable_vote import SingleTransferableVote
 from muoblpsolvers.solid_coalition_refinement import SolidCoalitionRefinement
 from muoblpsolvers.summed_objectives_lp_solver import SummedObjectivesLpSolver
+
+__all__ = [
+    "ExpandingApprovals",
+    "GreedySolver",
+    "MethodOfEqualSharesAdd1Solver",
+    "MethodOfEqualSharesConstrainsSolver",
+    "MethodOfEqualSharesExponentialSolver",
+    "MethodOfEqualSharesUtilitySolver",
+    "PhragmenSolver",
+    "SingleTransferableVote",
+    "SolidCoalitionRefinement",
+    "SummedObjectivesLpSolver",
+]
 
 logger = logging.getLogger(__name__)
 
